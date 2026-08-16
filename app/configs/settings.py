96cc9da617c80f555b4b9dbd337d33e3db24d5cb
Abhishekkrsingh2023 <abhishekkrsingh2023kolkata@gmail.com>
2026-08-17 00:41:40 +0530

    config file

---
+++ app/configs/settings.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    MONGO_URI: str = os.getenv("MONGO_URI")
+    MONGO_DB_NAME: str = os.getenv("MONGO_DB_NAME")
+    GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY")
