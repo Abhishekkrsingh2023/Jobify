9ea9f100e26cdd6fa2432b285b30fd9dc64ec487
Abhishekkrsingh2023 <abhishekkrsingh2023kolkata@gmail.com>
2026-08-20 18:39:33 +0530

    CORS

--- app/main.py
+++ app/main.py
@@ -35,13 +35,7 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
-origins = list({
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:3000",
-    settings.FRONTEND_URL,
-})
+origins = [settings.FRONTEND_URL]
 
 app.add_middleware(
     CORSMiddleware,
